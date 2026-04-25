ff2bd483aec1023b9ccb774d9beddadef9a19a0b
Perry He <hi@cdme.cn>
2026-04-25 10:36:18 +0800

    add visit_log

--- internal/ent/schema/visitlog.go
+++ internal/ent/schema/visitlog.go
@@ -1,15 +1,77 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+)
 
 // VisitLog holds the schema definition for the VisitLog entity.
 type VisitLog struct {
 	ent.Schema
 }
 
+func (VisitLog) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entsql.Annotation{Table: "visit_log"},
+		schema.Comment("Access log storage."),
+		entsql.WithComments(true),
+	}
+}
+
 // Fields of the VisitLog.
 func (VisitLog) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("ip").
+			MaxLen(45).
+			Comment("Client IP address"),
+
+		field.String("method").
+			MaxLen(10).
+			Comment("HTTP request method"),
+
+		field.String("path").
+			MaxLen(512).
+			Comment("Request URL path"),
+
+		field.Text("query").
+			Optional().
+			Comment("Query string"),
+
+		field.String("title").
+			MaxLen(255).
+			Optional().
+			Comment("Page title"),
+
+		field.Text("referer").
+			Optional().
+			Comment("Referrer URL"),
+
+		field.String("ua").
+			MaxLen(512).
+			Optional().
+			Comment("User-Agent string"),
+
+		field.Bool("is_bot").
+			Default(false).
+			Comment("Is bot/crawler"),
+
+		field.Time("created_at").
+			Default(time.Now).
+			Immutable().
+			Comment("Log timestamp"),
+	}
+}
+
+func (VisitLog) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("created_at"),
+		index.Fields("ip"),
+	}
 }
 
 // Edges of the VisitLog.
