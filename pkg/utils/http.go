3f636d95bcba6c4194b706196ae235d760dbda29
wantnotshould <hi@cdme.cn>
2026-04-25 19:56:51 +0800

    real ip (#6)
    * _ => -
    
    * real ip

--- pkg/utils/http.go
+++ pkg/utils/http.go
@@ -7,9 +7,25 @@ package utils
 import (
 	"net"
 	"net/http"
+	"strings"
 )
 
 func IP(r *http.Request) string {
+	if xri := r.Header.Get("X-Real-IP"); xri != "" {
+		return strings.TrimSpace(xri)
+	}
+
+	if xff := r.Header.Get("X-Forwarded-For"); xff != "" {
+		if before, _, ok := strings.Cut(xff, ","); ok {
+			return strings.TrimSpace(before)
+		}
+		return strings.TrimSpace(xff)
+	}
+
+	if xri := r.Header.Get("X-Real-IP"); xri != "" {
+		return strings.TrimSpace(xri)
+	}
+
 	host, _, err := net.SplitHostPort(r.RemoteAddr)
 	if err != nil {
 		return r.RemoteAddr
